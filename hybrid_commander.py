ad5232aa6e5cbcc62ea73a721053908ce1a70b15
Feng Shan <twinhorse@twinhorse.net>
2018-11-30 13:23:42 +0100

    big size field test

--- hybrid_commander.py
+++ hybrid_commander.py
@@ -7,7 +7,7 @@ from cflib.crazyflie.syncCrazyflie import SyncCrazyflie
 from cflib.positioning.motion_commander import MotionCommander
 from cflib.positioning.position_hl_commander import PositionHlCommander
 
-URI = 'radio://0/20/2M/E7E7E7E7E7'
+URI = 'radio://0/0/2M/E7E7E7E7E7'
 
 
 if __name__ == '__main__':
@@ -16,28 +16,34 @@ if __name__ == '__main__':
 
     with SyncCrazyflie(URI, cf=Crazyflie(rw_cache='./cache')) as scf:
       # We take off when the commander is create
-        mc = MotionCommander(scf)
-        pc = PositionHlCommander(scf)
+        with PositionHlCommander(scf) as pc:
+            pc.default_velocity=0.3,
         #print('to mc take off')
         #mc.take_off(1.2,0.3)
         #time.sleep(1)
-        print('to pc take off')
-        pc.take_off(1.75,0.3)
-        print('after take off')
+        #print(pc.get_position())
+        #print('to pc take off')
+        #pc.take_off(1.70,0.3)
+        #print(pc.get_position())
+        #print('after take off')
         #time.sleep(1.5)
         #mc.circle_right(0.5, velocity = 0.3,angle_degrees = 360)
         #time.sleep(1.5)
         #mc.land(0.2)
-        print('pc.go_to(1.5, 0, 1.75)')
-        pc.go_to(1.5, 0, 1.75)
-        print('pc.go_to(1.5, 0, 1.75)')
-        pc.go_to(1.5, 1.5, 1.75)
-        print('pc.go_to(0, 1.5, 1.75)')
-        pc.go_to(0, 1.5, 1.75)
-        print('pc.go_to(0, 0, 1.75)')
-        pc.go_to(0, 0, 1.75)
-        time.sleep(1)
-        print('to pc land')
-        pc.land(0.3)
-        print('landed')
+            print(pc.get_position())
+            print('pc.go_to(1.5, 0, 1.6)')
+            pc.go_to(0, 0, 1.6)
+            print(pc.get_position())
+            pc.go_to(2, 0, 1.6)
+            print(pc.get_position())
+            pc.go_to(2, 2, 1.6)
+            print(pc.get_position())
+            pc.go_to(0, 2, 1.6)
+            print(pc.get_position())
+            pc.go_to(0, 0, 1.6)
+            print(pc.get_position())
+        #print('to pc land')
+        #pc.land(0.3)
+        #print(pc.get_position())
+        #print('landed')
 
