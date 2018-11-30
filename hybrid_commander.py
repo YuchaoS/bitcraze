f2a0aa74db6d8ca93708b9ffd2605d5df44d48f3
Feng Shan <twinhorse@twinhorse.net>
2018-11-30 12:23:43 +0100

    first working

--- hybrid_commander.py
+++ hybrid_commander.py
@@ -7,7 +7,7 @@ from cflib.crazyflie.syncCrazyflie import SyncCrazyflie
 from cflib.positioning.motion_commander import MotionCommander
 from cflib.positioning.position_hl_commander import PositionHlCommander
 
-URI = 'radio://0/10/2M/E7E7E7E7E7'
+URI = 'radio://0/20/2M/E7E7E7E7E7'
 
 
 if __name__ == '__main__':
@@ -18,17 +18,26 @@ if __name__ == '__main__':
       # We take off when the commander is create
         mc = MotionCommander(scf)
         pc = PositionHlCommander(scf)
-        mc.take_off(1.2,0.3)
-        time.sleep(1)
-        pc.take_off(1.8,0.3)
+        #print('to mc take off')
+        #mc.take_off(1.2,0.3)
+        #time.sleep(1)
+        print('to pc take off')
+        pc.take_off(1.75,0.3)
+        print('after take off')
         #time.sleep(1.5)
         #mc.circle_right(0.5, velocity = 0.3,angle_degrees = 360)
         #time.sleep(1.5)
         #mc.land(0.2)
-        pc.go_to(2.0, 2.0, 1.0)
-        time.sleep(3)
-        print('to land')
+        print('pc.go_to(1.5, 0, 1.75)')
+        pc.go_to(1.5, 0, 1.75)
+        print('pc.go_to(1.5, 0, 1.75)')
+        pc.go_to(1.5, 1.5, 1.75)
+        print('pc.go_to(0, 1.5, 1.75)')
+        pc.go_to(0, 1.5, 1.75)
+        print('pc.go_to(0, 0, 1.75)')
+        pc.go_to(0, 0, 1.75)
+        time.sleep(1)
+        print('to pc land')
         pc.land(0.3)
         print('landed')
-   
 
