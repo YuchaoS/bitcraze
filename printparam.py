2dddd99349025aebf46d100422de8bbe9eb1eada
Feng Shan <twinhorse@twinhorse.net>
2018-12-01 08:46:36 +0100

    print parameters from crazyflie done

--- printparam.py
+++ printparam.py
@@ -8,29 +8,28 @@ from cflib.crazyflie.syncLogger import SyncLogger
 
 URI = 'radio://0/10/2M/E7E7E7E7E7'
 
+def position_callback(timestamp, data, logconf):
+    print(data)
+
+# 用这个方法可以直接定时打印实时参数，但是由于无人机的操作调用都是异步调用，所以必须增加一个线程来，实现实时打印。该线程必须设置结束条件。
+
+
 if __name__ == '__main__':
     # Initialize the low-level drivers (don't list the debug drivers)
     cflib.crtp.init_drivers(enable_debug_driver=False)
-
-    with SyncCrazyflie(uri, cf=Crazyflie(rw_cache='./cache')) as scf:
-        start_position_printing(scf)
-
-
-def position_callback(timestamp, data, logconf):
-    x = data['kalman.stateX']
-    y = data['kalman.stateY']
-    z = data['kalman.stateZ']
-    print('pos: ({}, {}, {})'.format(x, y, z))
-
-def start_position_printing(scf):
     log_conf = LogConfig(name='Position', period_in_ms=500)
     log_conf.add_variable('kalman.stateX', 'float')
     log_conf.add_variable('kalman.stateY', 'float')
     log_conf.add_variable('kalman.stateZ', 'float')
+    log_conf.add_variable('stabilizer.roll', 'float')
+    log_conf.add_variable('stabilizer.pitch', 'float')
+    log_conf.add_variable('stabilizer.yaw', 'float')
 
-    scf.cf.log.add_config(log_conf)
-    log_conf.data_received_cb.add_callback(position_callback)
-    log_conf.start()
-
-
+    with SyncCrazyflie(URI, cf=Crazyflie(rw_cache='./cache')) as scf:
+        scf.cf.log.add_config(log_conf)
+        log_conf.data_received_cb.add_callback(position_callback)
+        log_conf.start()
+        for i in range(60):
+            print(i)
+            time.sleep(0.5)
 
