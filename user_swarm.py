c51a1cd0c5375121ce5e31b2082599e0adae14b4
邵瑜超 <>
2018-12-18 18:08:49 +0800

    swarm

---
+++ user_swarm.py
@@ -0,0 +1,163 @@
+import time
+
+import cflib.crtp
+from cflib.crazyflie.log import LogConfig
+from cflib.crazyflie.swarm import CachedCfFactory
+from cflib.crazyflie.swarm import Swarm
+from cflib.crazyflie.syncLogger import SyncLogger
+
+URI1 = 'radio://0/0/2M/E7E7E7E701'
+URI2 = 'radio://0/0/2M/E7E7E7E702'
+URI3 = 'radio://0/0/2M/E7E7E7E703'
+
+sequence1 = [
+    (0.,0.,1.0,3.0),
+    (1.,1.,1.0,30.0),
+    (0.,2.,1.0,3.0),
+]
+sequence2 = [
+    (0.,0.,1.2,3.0),
+    (1.,1.,1.2,30.0),
+    (0.,2.,1.2,3.0),
+]
+sequence3 = [
+    (0.,0.,1.4,3.0),
+    (1.,1.,1.4,30.0),
+    (0.,2.,1.4,3.0),
+]
+seq_args = {
+    URI1: [sequence1],
+    URI2: [sequence2],
+    URI3: [sequence3],
+}
+uris = {
+    URI1,
+    URI2,
+    URI3,
+}
+
+def wait_for_position_estimator(scf):
+    print('Waiting for estimator to find position...')
+
+    log_config = LogConfig(name='Kalman Variance', period_in_ms=500)
+    log_config.add_variable('kalman.varPX', 'float')
+    log_config.add_variable('kalman.varPY', 'float')
+    log_config.add_variable('kalman.varPZ', 'float')
+
+    var_y_history = [1000] * 10
+    var_x_history = [1000] * 10
+    var_z_history = [1000] * 10
+
+    threshold = 0.001
+
+    with SyncLogger(scf, log_config) as logger:
+        for log_entry in logger:
+            data = log_entry[1]
+
+            var_x_history.append(data['kalman.varPX'])
+            var_x_history.pop(0)
+            var_y_history.append(data['kalman.varPY'])
+            var_y_history.pop(0)
+            var_z_history.append(data['kalman.varPZ'])
+            var_z_history.pop(0)
+
+            min_x = min(var_x_history)
+            max_x = max(var_x_history)
+            min_y = min(var_y_history)
+            max_y = max(var_y_history)
+            min_z = min(var_z_history)
+            max_z = max(var_z_history)
+
+            # print("{} {} {}".
+            #       format(max_x - min_x, max_y - min_y, max_z - min_z))
+
+            if (max_x - min_x) < threshold and (
+                    max_y - min_y) < threshold and (
+                    max_z - min_z) < threshold:
+                break
+
+
+def wait_for_param_download(scf):
+    while not scf.cf.param.is_updated:
+        time.sleep(1.0)
+    print('Parameters downloaded for', scf.cf.link_uri)
+
+
+def reset_estimator(scf):
+    cf = scf.cf
+    cf.param.set_value('kalman.resetEstimation', '1')
+    time.sleep(0.1)
+    cf.param.set_value('kalman.resetEstimation', '0')
+
+    wait_for_position_estimator(cf)
+
+
+def take_off(cf, position):
+    take_off_time = 1.0
+    sleep_time = 0.1
+    steps = int(take_off_time / sleep_time)
+    vz = position[2] / take_off_time
+
+    print(vz)
+
+    for i in range(steps):
+        cf.commander.send_velocity_world_setpoint(0, 0, vz, 0)
+        time.sleep(sleep_time)
+
+
+def land(cf, position):
+    landing_time = 1.0
+    sleep_time = 0.1
+    steps = int(landing_time / sleep_time)
+    vz = -position[2] / landing_time
+
+    print(vz)
+
+    for i in range(steps):
+        cf.commander.send_velocity_world_setpoint(0, 0, vz, 0)
+        time.sleep(sleep_time)
+
+    cf.commander.send_setpoint(0, 0, 0, 0)
+    # Make sure that the last packet leaves before the link is closed
+    # since the message queue is not flushed before closing
+    time.sleep(0.1)
+
+
+def run_sequence(scf, sequence):
+    try:
+        cf = scf.cf
+        cf.param.set_value('flightmode.posSet', '1')
+
+        take_off(cf, sequence[0])
+        for position in sequence:
+            print('Setting position {}'.format(position))
+            end_time = time.time() + position[3]
+            while time.time() < end_time:
+                cf.commander.send_setpoint(position[1], position[0], 0,
+                                           int(position[2] * 1000))
+                time.sleep(0.1)
+        land(cf, sequence[-1])
+    except Exception as e:
+        print(e)
+
+
+if __name__ == '__main__':
+    # logging.basicConfig(level=logging.DEBUG)
+    cflib.crtp.init_drivers(enable_debug_driver=False)
+
+    factory = CachedCfFactory(rw_cache='./cache')
+    with Swarm(uris, factory=factory) as swarm:
+        # If the copters are started in their correct positions this is
+        # probably not needed. The Kalman filter will have time to converge
+        # any way since it takes a while to start them all up and connect. We
+        # keep the code here to illustrate how to do it.
+        # swarm.parallel(reset_estimator)
+
+        # The current values of all parameters are downloaded as a part of the
+        # connections sequence. Since we have 10 copters this is clogging up
+        # communication and we have to wait for it to finish before we start
+        # flying.
+        print('Waiting for parameters to be downloaded...')
+        swarm.parallel(wait_for_param_download)
+
+        swarm.parallel(run_sequence, args_dict=seq_args)
