7a9287525a51a53d549a7b560086a96619c34d24
Feng Shan <twinhorse@twinhorse.net>
2019-03-07 10:57:30 +0800

    matlab and twrlog

---
+++ main.m
@@ -0,0 +1,17 @@
+LOCODECK_TS_FREQ=499.2e6*128;
+SPEED_OF_LIGHT=299792458;
+data=dlmread('lps_twr.log');
+tround1=data(:,1);
+tround2=data(:,2);
+treply1=data(:,3);
+treply2=data(:,4);
+dround1=data(:,1)/ LOCODECK_TS_FREQ*SPEED_OF_LIGHT;
+dround2=data(:,2)/ LOCODECK_TS_FREQ*SPEED_OF_LIGHT;
+dreply1=data(:,3)/ LOCODECK_TS_FREQ*SPEED_OF_LIGHT;
+dreply2=data(:,4)/ LOCODECK_TS_FREQ*SPEED_OF_LIGHT;
+tprop_ctn = ((tround1.*tround2)-(treply1.*treply2))./(tround1 + tround2 + treply1 + treply2);
+tprop = tprop_ctn / LOCODECK_TS_FREQ;
+distance = SPEED_OF_LIGHT * tprop;
+distance_quter=0.25*(tround1-treply1+tround2-treply2)/ LOCODECK_TS_FREQ*SPEED_OF_LIGHT;
+[data(:,5) distance (dround1-dreply1)/2 (dround2-dreply2)/2 (dround1-dreply1)/2-(dround2-dreply2)/2]
+
--- printparam.py
+++ printparam.py
@@ -6,7 +6,7 @@ from cflib.crazyflie.log import LogConfig
 from cflib.crazyflie.syncCrazyflie import SyncCrazyflie
 from cflib.crazyflie.syncLogger import SyncLogger
 
-URI = 'radio://0/60/2M/E7E7E7E7E7'
+URI = 'radio://0/40/2M/E7E7E7E7E7'
 
 def position_callback(timestamp, data, logconf):
     print(data)
@@ -15,8 +15,8 @@ if __name__ == '__main__':
     # Initialize the low-level drivers (don't list the debug drivers)
     cflib.crtp.init_drivers(enable_debug_driver=False)
     log_conf = LogConfig(name='Position', period_in_ms=1000)
-    log_conf.add_variable('ranging.distance0', 'float')
-   #log_conf.add_variable('kalman.stateX', 'float')
+    log_conf.add_variable('ranging.distance2', 'float')
+    #log_conf.add_variable('kalman.stateX', 'float')
     #log_conf.add_variable('kalman.stateZ', 'float')
     #log_conf.add_variable('stabilizer.roll', 'float')
     #log_conf.add_variable('stabilizer.pitch', 'float')
@@ -26,7 +26,7 @@ if __name__ == '__main__':
         scf.cf.log.add_config(log_conf)
         log_conf.data_received_cb.add_callback(position_callback)
         log_conf.start()
-        for i in range(20):
+        for i in range(60):
             print(i)
             time.sleep(1)
 
