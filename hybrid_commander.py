35ae3edc4f6084308deb6a8b13e73a22705b4459
xxxx <555555@qq.com>
2019-03-23 16:50:26 +0100

    2019/3/23

--- hybrid_commander.py
+++ hybrid_commander.py
@@ -2,13 +2,18 @@ import cflib.crtp
 import time
 import logging
 
-from cflib.crazyflie import Crazyflie
+from cflib.crazyflie.log import LogConfig
 from cflib.crazyflie.syncCrazyflie import SyncCrazyflie
+from cflib.crazyflie.syncLogger import SyncLogger
+
+from cflib.crazyflie import Crazyflie
 from cflib.positioning.motion_commander import MotionCommander
 from cflib.positioning.position_hl_commander import PositionHlCommander
 
-URI = 'radio://0/40/2M/E7E7E7E7E7'
+URI = 'radio://0/50/2M/E7E7E7E7E7'
 
+def position_callback(timestamp, data, logconf):
+    print(data)
 
 if __name__ == '__main__':
     # Initialize the low-level drivers (don't list the debug drivers)
@@ -18,19 +23,46 @@ if __name__ == '__main__':
       # We take off when the commander is create
         with PositionHlCommander(scf) as pc:
             print('after take off, to sleep 1s')
+            log_conf = LogConfig(name='Position', period_in_ms=500)
+            log_conf.add_variable('kalman.stateX', 'float')
+            log_conf.add_variable('kalman.stateY', 'float')
+            log_conf.add_variable('kalman.stateZ', 'float')
+            scf.cf.log.add_config(log_conf)
+            log_conf.data_received_cb.add_callback(position_callback)
+            log_conf.start()
+            cf = scf.cf
             time.sleep(1)
+            x=0.5
+            y=0.5
+            z=1.0
             pc.default_velocity=0.3
-            pc.go_to(1.0,1.0,1.0)
+            pc.go_to(-x,-y,0.3)
+            time.sleep(2)
+            print('1')
+            pc.go_to(-x,-y,z)
             time.sleep(1)
-            pc.go_to(2.0,1.0,1.0)
-            time.sleep(2)
-            pc.go_to(2.0,2.0,1.5)
-            time.sleep(2)
-            pc.go_to(1.0,2.0,2.0)
-            time.sleep(2)
-            pc.go_to(1.0,1.0,1.5)
-            time.sleep(2)
-            pc.go_to(1.0,1.0,0.3)
+            print('2')
+            pc.go_to(x,-y,z)
+            time.sleep(1)
+            #cf.commander.send_hover_setpoint(0, 0, 90, z)
+            #time.sleep(1)
+            print('3')
+            pc.go_to(x,y,z)
+            time.sleep(1)
+            #cf.commander.send_hover_setpoint(0, 0, 90, z)
+            #time.sleep(1)
+            print('4')
+            pc.go_to(-x,y,z)
+            time.sleep(1)
+            #cf.commander.send_hover_setpoint(0, 0, 90, z)
+            #time.sleep(1)
+            print('5')
+            pc.go_to(-x,-y,z)
+            time.sleep(1)
+            #cf.commander.send_hover_setpoint(0, 0, 90, z)
+            #time.sleep(1)
+            print('6')
+            pc.go_to(-1.0,-1.0,0.3)
             time.sleep(1)
             time.sleep(1)
            ## pc.go_to(2.0,1.5,0.2)
